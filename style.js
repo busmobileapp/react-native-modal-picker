f6e26d9f48f417e9b5beff8d1f443927fd1b34b4
sureshc <sureshc@99x.lk>
2017-11-18 17:59:58 +0530

    styles changed

--- style.js
+++ style.js
@@ -2,34 +2,23 @@
 
 import { StyleSheet, Dimensions } from 'react-native';
 
-const {height, width} = Dimensions.get('window');
-
-const PADDING = 8;
+const PADDING = 15;
 const BORDER_RADIUS = 5;
 const FONT_SIZE = 16;
-const HIGHLIGHT_COLOR = 'rgba(0,118,255,0.9)';
+const HIGHLIGHT_COLOR = '#758da3';
 const OPTION_CONTAINER_HEIGHT = 400;
 
 export default StyleSheet.create({
 
     overlayStyle: {
-        width: width,
-        height: height,
+        flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)'
     },
 
     optionContainer: {
-        borderRadius:BORDER_RADIUS,
-        width:width*0.8,
-        height:OPTION_CONTAINER_HEIGHT,
-        backgroundColor:'rgba(255,255,255,0.8)',
-        left:width*0.1,
-        top:(height-OPTION_CONTAINER_HEIGHT)/2
-    },
-
-    cancelContainer: {
-        left:width*0.1,
-        top:(height-OPTION_CONTAINER_HEIGHT)/2 + 10
+        flex: 1,
+        margin: 30,
+        backgroundColor:'#ffffff'
     },
 
     selectStyle: {
@@ -41,44 +30,28 @@ export default StyleSheet.create({
     },
 
     selectTextStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
-
-    cancelStyle: {
-        borderRadius: BORDER_RADIUS,
-        width: width * 0.8,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        padding: PADDING
-    },
-
-    cancelTextStyle: {
-        textAlign: 'center',
         color: '#333',
         fontSize: FONT_SIZE
     },
 
     optionStyle: {
         padding: PADDING,
-        borderBottomWidth: 1,
+        borderBottomWidth: 0.4,
         borderBottomColor: '#ccc'
     },
 
     optionTextStyle: {
-        textAlign: 'center',
         fontSize: FONT_SIZE,
         color: HIGHLIGHT_COLOR
     },
 
     sectionStyle: {
         padding: PADDING * 2,
-        borderBottomWidth: 1,
+        borderBottomWidth: 0.4,
         borderBottomColor: '#ccc'
     },
 
     sectionTextStyle: {
-        textAlign: 'center',
         fontSize: FONT_SIZE
     }
 });
